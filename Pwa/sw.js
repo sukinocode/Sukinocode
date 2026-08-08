3f7d6219271d7bf270d5f1db321d20c28ab26a38
sukinocode <cockids110@gmail.com>
2026-08-08 21:16:28 +0700

    Update sw.js

--- Pwa/sw.js
+++ Pwa/sw.js
@@ -1,1 +1,42 @@
+const CACHE_NAME = 'suki999-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '../index.html',
+  '../aset/suki1.png',
+  '../aset/suki2.png'
+];
 
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
